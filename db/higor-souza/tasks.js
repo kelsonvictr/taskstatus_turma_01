9b59868d74db43c27fdeb6a502b07c78e843f5a3
higorsouza <higor.andrade@hotmail.com>
2024-06-21 21:33:27 -0300

    Migrando a task de mongodb de inprogress para concluded

--- db/higor-souza/tasks.js
+++ db/higor-souza/tasks.js
@@ -1,5 +1,11 @@
 var higor_souza_tasks = [
     {
+        "name": "Curso Completo MongoDB 2023 [NoSQL do Básico ao Avançado!]",
+        "description": "Estudando mongodb para melhorar meu conhecimento sobre este banco.",
+        "responsible": "Higor Souza",
+        "status": "concluded"
+    },
+    {
         "name": "Java Continuous Integration-Delivery c. AWS e Github Actions",
         "description": "Curso que mostra como trabalhar com testes do código, CI/CD com github actions e AWS",
         "responsible": "Higor Souza",
